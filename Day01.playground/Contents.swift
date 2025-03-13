6c2b567beda82e0c1ac0ce897735cd46fc6bbb2f
Yeonah Shim <simya0528@naver.com>
2025-03-13 22:33:04 +0900

    Day01-if

--- Day01.playground/Contents.swift
+++ Day01.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var isDarkMode: Bool = true
+
+if(isDarkMode == true) {
+    print("다크모드 입니다.")
+}else {
+    print("다크모드 아닙니다.")
+}
+
+var title: String = isDarkMode == true ? "다크모드 입니다." : "다크모드가 아닙니다."
+
+print(title)
